8d9327faf0c673b747d1b87bb41fbb11bf5f6217
Eric Chan <eric88ordinary@gmail.com>
2021-03-15 21:31:21 +0800

    add spinner

---
+++ src/components/spinner/spinner.component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './spinner.css';
+
+export default function Spinner() {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
